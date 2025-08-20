734d82ff8bf102857be843d8c37ea4fe5222df50
Son3333 <147619426+Son3333@users.noreply.github.com>
2025-08-20 17:24:49 +0700

    Update LKG_delete_header.js

--- LKG_delete_header.js
+++ LKG_delete_header.js
@@ -1,6 +1,12 @@
-// Script Demo: Xóa một số header trong request
-delete $request.headers["X-RevenueCat-ETag"];
-delete $request.headers["X-Device-Info"];
-delete $request.headers["User-Agent"];
+// Script: Xoá header không cần thiết gửi đến RevenueCat
+let headers = $request.headers;
 
-$done({ headers: $request.headers });
+// Xoá mấy trường có thể gây phát hiện
+delete headers["x-revenuecat-etag"];
+delete headers["X-RevenueCat-ETag"];
+delete headers["If-None-Match"];
+delete headers["User-Agent"];
+delete headers["X-Device-User-Agent"];
+delete headers["X-Client-Version"];
+
+$done({ headers });
